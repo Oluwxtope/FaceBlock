341b36639d62e1fdb2639426286957417ecc175c
Adedotun Adeyemo <77558935+adedotun-a@users.noreply.github.com>
2022-05-29 12:04:09 -0400

    modified allowed extensions

--- api/app.py
+++ api/app.py
@@ -7,7 +7,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('HELLO WORLD')
 
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = '../public/images'
